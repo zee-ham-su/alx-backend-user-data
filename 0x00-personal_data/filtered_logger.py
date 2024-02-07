dd23276b644848318cc2c893ceda9da248ff40e4
zee-ham-su <hsufiian@yahoo.com>
2024-02-07 16:48:33 +0000

    database connection

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,6 +5,8 @@ returns the log message obfuscated:"""
 import re
 import logging
 from typing import List
+import os
+import mysql.connector
 
 
 def filter_datum(fields: str, redaction: str, message: str,
@@ -35,3 +37,33 @@ class RedactingFormatter(logging.Formatter):
             record.msg,
             self.SEPARATOR)
         return super().format(record)
+
+
+PII_FIELDS = ('name',
+              'email',
+              'phone',
+              'ssn',
+              'password')
+
+
+def get_logger() -> logging.Logger:
+    """returns a logging.Logger object"""
+    logger = logging.getLogger('user_data')
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    stream = logging.StreamHandler()
+    stream.setFormatter(RedactingFormatter(PII_FIELDS))
+    logger.addHandler(stream)
+    return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """returns a connector to the database"""
+
+    user = os.getenv('PERSONAL_DATA_DB_USERNAME', 'root')
+    password = os.getenv('PERSONAL_DATA_DB_PASSWORD', '')
+    host = os.getenv('PERSONAL_DATA_DB_HOST', 'localhost')
+    database_name = os.getenv('PERSONAL_DATA_DB_NAME')
+    connection = mysql.connector.connect(user=user, password=password,
+                                         host=host, database=database_name)
+    return connection
--- 0x00-personal_data/main.py
+++ 0x00-personal_data/main.py
@@ -3,12 +3,20 @@
 Main file
 """
 
-import logging
-import re
+import mysql.connector  # Import the MySQL connector module
 
-RedactingFormatter = __import__('filtered_logger').RedactingFormatter
+# Import the get_db function from filtered_logger module
+from filtered_logger import get_db
 
-message = "name=Bob;email=bob@dylan.com;ssn=000-123-0000;password=bobby2019;"
-log_record = logging.LogRecord("my_logger", logging.INFO, None, None, message, None, None)
-formatter = RedactingFormatter(fields=("email", "ssn", "password"))
-print(formatter.format(log_record))
+try:
+    # Attempt to establish a database connection
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("SELECT COUNT(*) FROM users;")
+    for row in cursor:
+        print(row[0])
+    cursor.close()
+    db.close()
+except mysql.connector.Error as err:
+    # Handle any errors that occur during the database connection process
+    print("Error connecting to the database:", err)
