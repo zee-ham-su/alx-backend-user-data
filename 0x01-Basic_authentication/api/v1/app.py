7ac587605e0eb1173bf772f01e86b693d546d7d0
zee-ham-su <hsufiian@yahoo.com>
2024-02-12 15:15:36 +0000

    Add authentication support to API

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -7,11 +7,34 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
+from api.v1.auth.auth import Auth
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+
+auth = None
+if os.getenv("AUTH_TYPE") == "auth":
+    auth = Auth()
+
+@app.before_request
+def before_request():
+    """ to filter each request
+    """
+    if auth is None:
+        pass
+    filtered_list = [
+        '/api/v1/status/',
+        '/api/v1/unauthorized/', '/api/v1/forbidden/'
+    ]
+    if request.path in filtered_list:
+        pass
+    if auth.require_auth(request.path, filtered_list):
+        if auth.authorization_header(request) is None:
+            abort(401)
+        if auth.current_user(request) is None:
+            abort(403)
 
 
 @app.errorhandler(404)
--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -26,7 +26,11 @@ class Auth():
     def authorization_header(self, request=None) -> str:
         """ gets the authentication header from the request
         """
-        return (None)
+        if request is None:
+            return None
+        if "Authorization" not in request.headers:
+            return None
+        return request.headers["Authorization"]
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ gets the current user from th request
