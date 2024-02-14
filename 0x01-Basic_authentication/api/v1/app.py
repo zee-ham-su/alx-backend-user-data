00a3998e7c5168bea2d347766d8b399b9778acf4
zee-ham-su <hsufiian@yahoo.com>
2024-02-14 21:54:20 +0000

    Remove session authentication and add session authentication to app.py

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -9,7 +9,6 @@ from flask_cors import (CORS, cross_origin)
 import os
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
-from api.v1.auth.session_auth import SessionAuth
 
 
 app = Flask(__name__)
@@ -22,8 +21,6 @@ if AUTH_TYPE == "auth":
     auth = Auth()
 elif AUTH_TYPE == "basic_auth":
     auth = BasicAuth()
-elif AUTH_TYPE == "session_auth":
-    auth = SessionAuth()
 
 
 @app.before_request
