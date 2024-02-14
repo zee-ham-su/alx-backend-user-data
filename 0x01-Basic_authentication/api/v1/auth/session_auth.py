4c5750f9997875f4bbe77dd1b6321356832a4c9f
zee-ham-su <hsufiian@yahoo.com>
2024-02-14 21:50:18 +0000

    Add session authentication support

---
+++ 0x01-Basic_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Session authentication module
+"""
+from models.user import User
+from .auth import Auth
+
+
+class SessionAuth(Auth):
+    """SessionAuth class"""
+    pass
