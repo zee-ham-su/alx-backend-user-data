b489a6161d714b7f083ccfe4ac57809352f40a3f
zee-ham-su <hsufiian@yahoo.com>
2024-02-15 11:28:11 +0000

    Add session authentication for login endpoint

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -34,10 +34,13 @@ def before_request():
         pass
     else:
         filtered_list = ['/api/v1/status/',
-                         '/api/v1/unauthorized/', '/api/v1/forbidden/']
+                         '/api/v1/unauthorized/',
+                         '/api/v1/forbidden/',
+                         '/api/v1/auth_session/login/']
 
         if auth.require_auth(request.path, filtered_list):
-            if auth.authorization_header(request) is None:
+            if auth.authorization_header(
+                    request) is None and auth.session_cookie(request) is None:
                 abort(401, description="Unauthorized")
             if auth.current_user(request) is None:
                 abort(403, description='Forbidden')
