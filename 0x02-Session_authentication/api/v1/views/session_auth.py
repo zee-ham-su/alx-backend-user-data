6ada63f54661ce46178a7beb29b6050cfcc833e9
zee-ham-su <hsufiian@yahoo.com>
2024-02-15 18:12:32 +0000

    Update session authentication and user data***

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -1,34 +1,33 @@
 #!/usr/bin/env python3
-"""new Flask view that handles all routes
-for the Session authentication
+""" Module of Users views
 """
+import os
+from flask import jsonify, request
 from api.v1.views import app_views
-from flask import jsonify, request, abort
 from models.user import User
 
 
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
-def handle_session_auth():
-    """ POST /auth_session/login
+def auth_session():
+    """POST /auth_session/login
     """
     email = request.form.get('email')
     password = request.form.get('password')
 
-    if email is None or email == "":
+    if email is None or email == '':
         return jsonify({"error": "email missing"}), 400
-    if password is None or password == "":
+    if password is None or password == '':
         return jsonify({"error": "password missing"}), 400
-
-    user = User.search({'email': email})
-    if user is None or user == []:
+    users = User.search({"email": email})
+    if not users or users == []:
         return jsonify({"error": "no user found for this email"}), 404
 
-    user = user[0]
-    if not user.is_valid_password(password):
-        return jsonify({"error": "wrong password"}), 401
-
-    from api.v1.auth.session_auth import SessionAuth
-    session = SessionAuth().create_session(user.id)
-    data = jsonify(user.to_json())
-    data.set_cookie('session_id', session)
-    return data
+    for user in users:
+        if user.is_valid_password(password):
+            from api.v1.app import auth
+            session_id = auth.create_session(user.id)
+            session_response = jsonify(user.to_json())
+            session_name = os.getenv('SESSION_NAME')
+            session_response.set_cookie(session_name, session_id)
+            return session_response
+    return jsonify({"error": "wrong password"}), 401
