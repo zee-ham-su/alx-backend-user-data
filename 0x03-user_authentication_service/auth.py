1231e59943f02c79c8addfab0f2f19132f501c79
zee-ham-su <hsufiian@yahoo.com>
2024-02-19 16:01:42 +0000

    pycodestyle

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -37,6 +37,8 @@ class Auth:
         """
         try:
             user = self._db.find_user_by(email=email)
-            return bcrypt.checkpw(password.encode('utf-8'), user.hashed_password)
+            return bcrypt.checkpw(
+                password.encode('utf-8'),
+                user.hashed_password)
         except NoResultFound:
             return False
