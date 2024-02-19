3e36de139d651dd4d045d33bae62fef594f08104
zee-ham-su <hsufiian@yahoo.com>
2024-02-19 12:35:59 +0000

    basic flask app

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" basic flask app"""
+from flask import Flask, jsonify
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    data = {
+        "message": "Bienvenue"
+    }
+    return jsonify(data)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -19,7 +19,7 @@ class Auth:
 
     def __init__(self):
         self._db = DB()
-    
+
     def register_user(self, email: str, password: str) -> User:
         """ register_user method
         """
